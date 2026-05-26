28aee9cb2c6976db04f6f0eab9f45ddd2b4f4d53
Katya López <lopezkatya2009@gmail.com>
2026-05-26 10:04:20 -0600

    Este es mi archivo de aprendizaje ciclos

---
+++ ciclos.py
@@ -0,0 +1,17 @@
+#Ciclo, interacion, bucle
+
+#while
+"""
+i = 0
+while i < 10:
+    if i < 5:
+        print("El numero ",  i, "es menor a 5")
+    else:
+        print("El numero",  i, "es mayor o igual a 5")
+    i += 1 
+
+print("Terminó la interación")
+"""
+
+for x in "Katya":
+    print(X)
