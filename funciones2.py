a52d9cb51a1f7d42f30a2c583c502cbc5df64fb3
Katya López <lopezkatya2009@gmail.com>
2026-05-27 11:56:12 -0600

    Mi archivo de funciones

---
+++ funciones2.py
@@ -0,0 +1,8 @@
+def saludar_y_sumar(nombre, num1, num2=0):
+    print("Saludos", nombre)
+    print("Resultado de la suma", num1+num2)
+
+saludar_y_sumar("Katya", 14, 8) 
+saludar_y_sumar("Ana", 23)
+saludar_y_sumar("Lisseth", 27, 28)
+   
