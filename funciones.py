c5fca396c2885ab14f55db24ef88b281ab65344d
Katya López <lopezkatya2009@gmail.com>
2026-05-27 11:46:14 -0600

    Esta es mi modificación

---
+++ funciones.py
@@ -0,0 +1,16 @@
+#Pre-definidas print(), type(), range(), input()
+
+def saludar(nombre):
+    print("Hola", nombre)
+
+#Celsius a Fahrenheit: (C * 1.8) + 32
+def convertir_a_fahrenheit(c):
+    return (C * 1.8) + 32
+
+#saludar("Katya")
+#saludar("Ana") 
+#saludar("Lisseth")
+
+print( convertir_a_fahrenheit(10) )
+print( convertir_a_fahrenheit(30) )
+print( convertir_a_fahrenheit(80) )
