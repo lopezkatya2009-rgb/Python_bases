a450d0cf3b18b9c7cb6cb2622f94ddc1b910b168
Katya López <lopezkatya2009@gmail.com>
2026-05-27 18:58:37 -0600

    Mis modificaciones

--- funciones2.py
+++ funciones2.py
@@ -4,5 +4,4 @@ def saludar_y_sumar(nombre, num1, num2=0):
 
 saludar_y_sumar("Katya", 14, 8) 
 saludar_y_sumar("Ana", 23)
-saludar_y_sumar("Lisseth", 27, 28)
-   
+saludar_y_sumar("Lisseth", 27, 28) 
